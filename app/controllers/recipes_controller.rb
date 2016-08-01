1cae7f8263e1c6e1c0cc863afd0de53a27108851
Mark-Nelson <mark.nelson@dragonicity.com>
2016-08-01 16:42:34 +0800

    Add new form and processing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,5 +8,27 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find(1)
+
+    if @recipe.save
+      flash[:success] = "Recipe was created successfully"
+      redirect_to recipes_path
+    else
+      render 'new'
+    end
+
+  end
+
+  private
+
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
 
 end
