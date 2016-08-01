57626056078f7b820b53f37823ea1670c419a4b7
Mark-Nelson <mark.nelson@dragonicity.com>
2016-08-01 13:59:52 +0800

    Add tests for chef and recipe

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,13 +3,19 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
 
   def setup
-    @recipe = Recipe.new(name: "Chicken parm", 
+    @chef = Chef.create(name: "Mark", email: "Mark@example.com")
+    @recipe = @chef.recipes.build(name: "Chicken parm", 
       summary: "The best chicken parm recipe ever",
       description: "Heat oil, add onions, add chicken, etc.")
   end
 
   test "recipe should be valid" do 
     assert @recipe.valid?
+  end
+
+  test "chef_id should be present" do 
+    @recipe.chef_id = nil
+    assert_not @recipe.valid?
   end
 
   test "name should be present" do 
