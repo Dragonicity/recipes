834c4db64b89ac45e15665f2d1172fe19f3b26fe
Mark-Nelson <mark.nelson@dragonicity.com>
2016-08-01 19:41:41 +0800

    Add validations for likes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
 
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all.sort_by{ |likes| likes.thumbs_up_total }.reverse
   end
 
   def show
@@ -42,9 +42,14 @@ class RecipesController < ApplicationController
 
   def like
     @recipe = Recipe.find(params[:id])
-    Like.create(likes: params[:likes], chef: Chef.first, recipe: @recipe)
-    flash[:success] = "Your selection was recorded"
-    redirect_to :back
+    like = Like.create(likes: params[:likes], chef: Chef.first, recipe: @recipe)
+    if like.valid?
+      flash[:success] = "Your selection was recorded"
+      redirect_to :back
+    else
+      flash[:danger] = "You can only like/dislike a recipe once"
+      redirect_to :back
+    end
   end
 
   private
