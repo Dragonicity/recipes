978d98fb4ca330172e636db32000906676fe72f1
Mark-Nelson <mark.nelson@dragonicity.com>
2016-08-01 16:59:49 +0800

    Add edit form and oricessing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,6 +12,10 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.chef = Chef.find(1)
@@ -20,9 +24,20 @@ class RecipesController < ApplicationController
       flash[:success] = "Recipe was created successfully"
       redirect_to recipes_path
     else
-      render 'new'
+      render :new
     end
 
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    if @recipe.update(recipe_params)
+      flash[:success] = "Recipe was updated successfully"
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
   end
 
   private
