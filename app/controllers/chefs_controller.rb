76398bed2db70abc4385613e2e02ca345c22aa8a
Mark-Nelson <mark.nelson@dragonicity.com>
2016-08-02 10:52:35 +0800

    Add edit chef function

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,27 +1,35 @@
 class ChefsController < ApplicationController
 
+  def show
+    @chef = Chef.find(params[:id])
+  end
+
   def new
     @chef = Chef.new
   end
 
+  def edit
+    @chef = Chef.find(params[:id])
+  end
+
   def create
     @chef = Chef.new(chef_params)
-
     if @chef.save
       flash[:success] = "Chef was created registered"
       redirect_to recipes_path
     else
       render :new
     end
-
-  end
-
-  def edit
-
   end
 
   def update
-
+    @chef = Chef.find(params[:id])
+    if @chef.update(chef_params)
+      flash[:success] = "Chef was updated successfully"
+      redirect_to chef_path(@chef)
+    else
+      render :edit
+    end
   end
 
   private
