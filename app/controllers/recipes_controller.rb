17c8053fdac30bc3fa7f99d18d00bc9aba25cce3
Mark-Nelson <mark.nelson@dragonicity.com>
2016-08-03 14:20:50 +0800

    Add admin processing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,6 +4,7 @@ class RecipesController < ApplicationController
   before_action :require_user, except: [:show, :index, :like]
   before_action :require_user_like, only: [:like]
   before_action :require_same_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @recipes = Recipe.paginate(page: params[:page], per_page: 4)
@@ -41,6 +42,12 @@ class RecipesController < ApplicationController
     end
   end
 
+  def destroy
+    Recipe.find(params[:id]).destroy
+    flash[:success] = "Recipe was deleted successfully"
+    redirect_to recipes_path
+  end
+
   def like
 
     like = Like.create(likes: params[:likes], chef: current_user, recipe: @recipe)
@@ -71,10 +78,14 @@ class RecipesController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @recipe.chef
+      if current_user != @recipe.chef && !current_user.admin?
         flash[:danger] = "You can only edit your own recipes"
         redirect_to recipes_path
       end
+    end
+
+    def admin_user
+      redirect_to recipes_path unless current_user.admin?
     end
 
 end
